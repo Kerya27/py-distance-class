56c9af257f5b6c3860a4a92ffb68837619d7ab3d
Kyrylo Cherdakliiev <yungkeryaa@gmail.com>
2025-11-24 13:00:44 +0100

    Added __future__ annotations, dropped double uotes and added float to argument annotaitons in magical methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Callable, Any
 
 
@@ -9,7 +11,7 @@ class Distance:
     def other_value_parser(func: Callable) -> Callable:
         """Декоратор для вилучення числового значення з операнда 'other'."""
 
-        def wrapper(self: Any, other: "Distance | int") -> Any:
+        def wrapper(self: Any, other: Distance | int) -> Any:
             other_value = other
             if isinstance(other, Distance):
                 other_value = other.km
@@ -22,48 +24,48 @@ class Distance:
         return wrapper
 
     def __str__(self) -> str:
-        return f'Distance: {self.km} kilometers.'
+        return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
     @other_value_parser
-    def __add__(self, other: "Distance | int") -> "Distance":
+    def __add__(self, other: Distance | int | float) -> "Distance":
         return Distance(self.km + other)
 
-    def __iadd__(self, other: "Distance | int | float") -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, int | float):
+        elif isinstance(other, (int , float)):
             self.km += other
         else:
             print(f"{type(other)} not supported")
         return self
 
     # @other_value_parser
-    def __mul__(self, other: int) -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
     # @other_value_parser
-    def __truediv__(self, other: int) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round((self.km / other), 2))
 
     @other_value_parser
-    def __lt__(self, other: "Distance | int") -> bool:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self.km < other
 
     @other_value_parser
-    def __gt__(self, other: "Distance | int") -> bool:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self.km > other
 
     @other_value_parser
-    def __le__(self, other: "Distance | int") -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self.km <= other
 
     @other_value_parser
-    def __ge__(self, other: "Distance | int") -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self.km >= other
 
     @other_value_parser
-    def __eq__(self, other: "Distance | int") -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self.km == other
